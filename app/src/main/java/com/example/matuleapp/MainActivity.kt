8fabc45aa511e223d8463a49643cebd3915d8905
WSR_5 <annadenisova65@gmail.com>
2025-03-11 12:27:32 +0800

    Push

--- app/src/main/java/com/example/matuleapp/MainActivity.kt
+++ app/src/main/java/com/example/matuleapp/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.example.matuleapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.matuleapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.signInBtn.setOnClickListener {
+            val intent = Intent(this,HomeActivity::class.java)
+            startActivity(intent)
+        }
         }
     }
